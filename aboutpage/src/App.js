d987128b41c57d370d0abba490ba9752f3c529a5
Lawrence Warren <psyljw@nottingham.ac.uk>
2019-12-06 19:04:09 +0000

    Started getting some functionality out of the buttons, this language is the bane of my life

--- aboutpage/src/App.js
+++ aboutpage/src/App.js
@@ -22,34 +22,60 @@ class AboutPage extends React.Component
   constructor(props)
   {
     super(props);
-
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() //Handles button clicks - sets new picture src. and alts.
+  leftClick(index, imageArray)
   {
-    document.getElementById("picture").src = "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg";
-    document.getElementById("picture").alt = "Nandos Peri Peri Chicken restaurant";
+    if(index > 0)
+    {
+      index--;
+    }
+
+    document.getElementById("picture").src = imageArray[index];
+    return index;
+  }
+
+  rightClick(index, imageArray)
+  {
+    if(index < 3)
+    {
+      index++;
+    }
+
+    document.getElementById("picture").src = imageArray[index];
+    return index;
   }
 
   render()  //Render function.
   {
+    let index = 0;
+    //`let` does allow reassignment
+
+    //`const` does not allow reassignment
+    const imageArray = ["https://twistedsifter.files.wordpress.com/2012/09/art-deco-mcdonalds-clifton-hill-victoria-australia.jpg","https://www.pymnts.com/wp-content/uploads/2018/07/shutterstock_497368633.jpg","https://www.bunkwings.com/wp-content/uploads/2015/07/bunk-cocktails-and-wings-nottingham172.jpg", "https://media-cdn.tripadvisor.com/media/photo-s/0b/54/56/b7/aldershot-nando-s.jpg"]
+    const descriptionArray = ["McDonalds fast food restaurant","Pizza Hut Pizza restaurant","Bunk bar - chicken wings and cocktails", "Nandos peri peri chicken restaurant"]
+
+
     return(
       <div id="root" className="About">
-  
         <header className="App-header">
   
+
+        
           <img id="logo" src = "https://www.avas-angels.com/images/HiResLogo.png" alt="Ava's Angels logo"/>
   
-          <img id="picture" src="https://www.bunkwings.com/wp-content/uploads/2015/07/bunk-cocktails-and-wings-nottingham172.jpg" className="picture" alt="Bunk bar" />{document.getElementById("picture")}
+
+          <img id="picture" src= { imageArray[index] } className="picture" alt="Bunk bar" />{document.getElementById("picture")}
   
-          <button onClick={this.handleClick} className="right-button"> > </button>
-          <button onClick={this.handleClick} className="left-button"> &lt; </button>
+
+          <button onClick={this.rightClick.bind(this, index, imageArray)} className="right-button"> > </button>
+          <button onClick={this.leftClick.bind(this, index, imageArray)} className="left-button"> &lt; </button>
   
           <p id="text"> restaurant! </p>
   
+
+
         </header>
-  
       </div>
     );
   }
